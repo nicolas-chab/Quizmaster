ce654fad7c725e3b68a8fe55f45c93c93f187c82
nicolas-chab <101563616+nicolas-chab@users.noreply.github.com>
2025-08-01 20:02:03 -0300

    update2

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,14 +1,36 @@
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
+using UnityEngine.UI;
+
 public class Quiz : MonoBehaviour
 {
     [SerializeField]TextMeshProUGUI questiontext;
     [SerializeField] QuestionSO question;
+    [SerializeField]GameObject[] answerButton;
+    int correctanswerindex;
+    [SerializeField]Sprite defaultanswersprite;
+    [SerializeField] Sprite correctanswersprite;
     void Start()
     {
         questiontext.text = question.GetQuestion();
+
+        for (int i = 0; i < answerButton.Length; i++)
+        {
+            TextMeshProUGUI buttontext= answerButton[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttontext.text=  question.GetAnswer(i);
+        }
+        
     }
 
-   
+    public void OnAnswerSelected(int index)
+    {
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questiontext.text = "correct";
+            Image buttonimage = answerButton[index].GetComponent<Image>();
+            buttonimage.sprite = correctanswersprite;
+        }
+    }
     
 }
